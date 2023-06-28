f6acedc4998676913b5e0a9352d759253ace07ee
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2023-06-28 10:41:41 -0500

    3rd commit

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/rrq/golang/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(155)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	// estado, texto := variables.ConviertoaTexto(155)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("esto no es windows")
+	} else {
+		fmt.Println("esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Println("%s \n", os)
+	}
 }
